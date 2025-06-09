26b53174dbf821b6677aa8707ae8e876d713d128
Muhamad Jajuni <Muhamadjajuni98@gmail.com>
2025-06-09 13:32:51 +0700

    [ADD] - fix cors server.js

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 import db from "./src/models/index.js";
 import userRoutes from "./src/routes/userRoutes.js";
-import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
+
+// Pindahkan cors() ke sini, sebelum routes
+app.use(
+  cors({
+    origin: "http://localhost:5173", // izinkan hanya frontend kamu
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(userRoutes);
 
 const PORT = process.env.PORT || 3000;
